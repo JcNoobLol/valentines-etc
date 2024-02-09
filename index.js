9a81a83519dd13c42a162dc22bbfce3f4a76aa45
James Clerick <92348773+JcNoobLol@users.noreply.github.com>
2024-02-09 18:35:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let express  = require("express");let app = express(); app.listen(3000); console
 app.get('/',(req,res) => {
     res.sendfile(path.join(__dirname,'src/val.html'))
 })
+app.get("/14",(req,res) => {
+    res.sendfile(path.join(__dirname,'src/14.html'))
+})
